7bfb0e6d8d01b32db03b64fdf20a643238356e95
danyanyam <dvbuchko@edu.hse.ru>
2022-03-16 22:24:03 +0300

    catboost update

--- generate_submission.py
+++ generate_submission.py
@@ -1,23 +1,37 @@
 import numpy as np
+import pandas as pd
 from typing import Dict as D
 from pymatgen.core import Structure
+from catboost import CatBoostRegressor, Pool
 
 from models import prepare_model, MEGNetModel
-from utils import read_config, read_structures, write_csv
+from utils import read_config, read_structures
 
 
 def main():
     # get config with paths
-    config: D[str, str] = read_config('config.yaml')
+    config: D[str, str] = read_config("config.yaml")
     # reading pymatgen structures from path
-    private: D[str, Structure] = read_structures(config['data']['private']['defects'])
+    private: D[str, Structure] = read_structures(config["data"]["private"]["defects"])
     # initializing model
-    model: MEGNetModel = prepare_model(config['model']['weights'])
+    model: MEGNetModel = prepare_model(config["model"]["megnet"])
     # calculating predictions
     predictions: np.array = model.predict_structures(private.values())
-    result = [{'id': id_, 'predictions': pred[0]} for id_, pred in zip(private.keys(), predictions)]
+    result = [{"id": id_, "predictions": pred[0]} for id_, pred in zip(private.keys(), predictions)]
     # saving prediction
-    write_csv(result, 'submission.csv', ['id', 'predictions'])
+    megnet_predictions = pd.DataFrame(result).set_index('id')
+
+    data = pd.read_csv(config["data"]["private"]["features"], index_col=0).merge(megnet_predictions,
+                                                                                 left_index=True,
+                                                                                 right_index=True)
+    prediction_pool = Pool(data=data)
+    model = CatBoostRegressor().load_model(config["model"]["boosting"])
+
+    predictions = model.predict(prediction_pool)
+    results = pd.DataFrame(index=data.index, columns=["predictions"])
+    results["predictions"] = predictions
+    results.index.rename("id", inplace=True)
+    results.to_csv("submission.csv")
 
 
 if __name__ == "__main__":
